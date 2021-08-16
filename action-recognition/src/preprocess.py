37c496ce1bbb731920e5107cf43c786cde0dec67
Felipe Ferreira <feliferrgo@gmail.com>
2021-08-16 14:34:43 -0300

    added requirements.txt and some code

---
+++ action-recognition/src/preprocess.py
@@ -0,0 +1,65 @@
+from operator import index
+import pandas as pd
+import joblib
+import os
+import numpy as np
+
+from tqdm import tqdm
+from sklearn.preprocessing import LabelBinarizer
+
+def run(root_path, output_path):
+    all_paths = os.listdir(root_path)
+    folder_paths = [folder_path for folder_path in all_paths if os.path.isdir(os.path.join(root_path, folder_path))]
+    print(f"Folder paths: {folder_paths}")
+    print(f"Number of folders :{len(folder_paths)}")
+
+    create_labels = ["basketball", "boxing", "chess"]
+
+    data = pd.DataFrame()
+
+    image_formats = ['jpg', 'JPG', 'PNG', 'png']
+    labels = []
+    counter = 0
+    for i, folder_path in tqdm(enumerate(folder_paths), total=len(folder_paths)):
+        if folder_path not in create_labels:
+            continue
+        image_paths = os.listdir(os.path.join(root_path, folder_path))
+        label = folder_path
+
+        for image_path in image_paths:
+            if image_path.split('.')[-1] in image_formats:
+                data.loc[counter, 'image_path'] = f"{root_path}/{folder_path}/{image_path}"
+                labels.append(label)
+                counter += 1
+
+    labels = np.array(labels)
+    lb = LabelBinarizer()
+    labels = lb.fit_transform(labels)
+
+    print(labels.shape)
+    if len(labels[0]) == 1:
+        for i in range(len(labels)):
+            index = labels[i]
+            data.loc[i, 'target'] = int(index)
+    elif len(labels[0]) > 1:
+        for i in range(len(labels)):
+            index = np.argmax(labels[i])
+            data.loc[i, 'target'] = int(index)
+    
+    # shuffle the dataset
+    data = data.sample(frac=1).reset_index(drop=True)
+
+    print(f"Number of classes: {len(lb.classes_)}")
+    print(f"Total instances: {len(data)}")
+
+    # save to csv
+    data.to_csv(os.path.join(root_path,"data.csv"), index=False)
+
+    # saving the binarized labels
+    print("Saving the binarized labels in pickled file")
+    joblib.dump(lb, os.path.join(output_path, "lb.pkl"))
+
+    print(data.head(5))
+
+if __name__ == "__main__":
+    run("/Users/felipe.ferreira/dev/git/personal/computer-vision/action-recognition/input/data","/Users/felipe.ferreira/dev/git/personal/computer-vision/action-recognition/output")
